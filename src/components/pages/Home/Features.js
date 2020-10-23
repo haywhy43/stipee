568519069b2b05bc0e4036a03c9f2431ce105fee
haywhy43 <170805005@live.unilag.edu.ng>
2020-10-23 16:51:21 +0100

    update

---
+++ src/components/pages/Home/Features.js
@@ -0,0 +1,143 @@
+import React, { useEffect } from "react";
+import Button from "../../UI/button";
+import { graphql, useStaticQuery } from "gatsby";
+import styled from "styled-components";
+import Img from "gatsby-image";
+import Stipee from "../../SVG/stipee";
+
+const StyledCard = styled.div`
+  background: #ffffff;
+  border: 1px solid #dddde3;
+  box-shadow: 0px 20px 30px 5px rgba(0, 0, 0, 0.03);
+  border-radius: 10px;
+
+  p {
+    line-height: 30px;
+
+    @media (max-width: 640px) {
+      line-height: 26px;
+    }
+  }
+`;
+
+const features = [
+  {
+    img: "smiley",
+    alt: "smiley face",
+    title: "Fund stipends with ease",
+    body:
+      "With stipee parents can monitor their children's stipend in different schools. Prefund stipends with ease using stipee.",
+  },
+  {
+    img: "money",
+    alt: "dollars with wings emoji",
+    title: "Control child's spending",
+    body:
+      "Manage spending by setting limits, which would yield more insight into spending habits increasing financial intelligence.",
+  },
+  {
+    img: "card",
+    alt: "personalized card",
+    title: "Personalized Stipee card",
+    body:
+      "Kids can enjoy cashless and secured spending at school, eliminating queues with our Tap to pay personalized cards.",
+  },
+  {
+    img: "laptop",
+    alt: "laptop",
+    title: "Database of students for schools",
+    body:
+      "For free, a comprehensive electronic database of students would be provided to plan, budget and grow a business.",
+  },
+];
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      smiley: file(relativePath: { eq: "smiley.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      money: file(relativePath: { eq: "money.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      card: file(relativePath: { eq: "card.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      laptop: file(relativePath: { eq: "laptop.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  const eventListener = () => {
+    const doc = document.querySelector("#stipee");
+    const docPos = doc.getBoundingClientRect();
+
+    doc.style.transform = `translateX(-${(docPos.width / docPos.top) * 15}px)`;
+
+    console.log(docPos);
+  };
+
+  document.addEventListener("scroll", eventListener);
+
+  useEffect(() => {
+    return function cleanup() {
+      document.removeEventListener("scroll", eventListener);
+    };
+  });
+
+  return (
+    <section>
+      <div className="lg:flex w-11/12 md:w-10/12 lg:w-11/12 px-2 md:px-4 mx-auto relative">
+        <div className="xl:w-6/12 w-8/12">
+          <h3 className="text-3xl md:text-4xl text-font font-semibold">
+            Some of our <br /> features
+          </h3>
+          <div className="mt-4">
+            <Button text="Learn more" icon="arrow-right" />
+          </div>
+        </div>
+        <div className="mt-8 lg:mt-0 z-50">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 xl:gap-10 lg:w-11/12 ml-auto">
+            {features.map(feat => (
+              <StyledCard className="py-10 px-8 col-span-1" key={feat.title}>
+                <div className="w-12">
+                  <Img
+                    fluid={data[feat.img].childImageSharp.fluid}
+                    alt={feat.alt}
+                  />
+                </div>
+                <h4 className="text-base font-semibold text-font mt-5">
+                  {feat.title}
+                </h4>
+                <p className="text-base text-font-sub mt-3">{feat.body}</p>
+              </StyledCard>
+            ))}
+          </div>
+        </div>
+        <div className="absolute bottom-0 left-0 pb-8" id="stipee">
+          <div className="hidden lg:block">
+            <Stipee />
+          </div>
+        </div>
+      </div>
+      <div className="h-32 w-full"></div>
+    </section>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "../components/layout";
 import Header from "../components/pages/Home/Header";
 import Info from "../components/pages/Home/Info";
+import Features from "../components/pages/Home/Features";
 
 export default function Home() {
   return (
     <Layout>
       <Header />
       <Info />
+      <Features />
     </Layout>
   );
 }
